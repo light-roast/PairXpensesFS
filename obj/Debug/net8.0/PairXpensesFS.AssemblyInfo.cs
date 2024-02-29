ef7f60aa7de4d23dee44d417c8cc66dfdeddce13
Daniel Echeverri <echeverri121@gmail.com>
2024-02-29 10:18:29 -0500

    Style adjustments to other pages

--- obj/Debug/net8.0/PairXpensesFS.AssemblyInfo.cs
+++ obj/Debug/net8.0/PairXpensesFS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PairXpensesFS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+052d14003f946af0fe3a5b048bd9c9c7295741a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32cb86c6d3a07fe392a798b01084c66691c866d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("PairXpensesFS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PairXpensesFS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
